b997b562fcb84c19ef7a2e78c546364092df6026
rkdwlss2 <rkdwlss2@naver.com>
2021-05-05 14:30:55 +0900

    Initial Commit

---
+++ boj2437/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  boj2437
+//
+//  Created by 강명진 on 2021/05/05.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
