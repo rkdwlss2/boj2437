94a5ca5b6bd441020ac1acf0753d30f6e8079616
rkdwlss2 <rkdwlss2@naver.com>
2021-05-17 23:19:23 +0900

    dd

---
+++ boj3046.cpp
@@ -0,0 +1,17 @@
+//
+//  boj3046.cpp
+//  boj2437
+//
+//  Created by 강명진 on 2021/05/17.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int a,b;
+    cin>>a>>b;
+    cout<<2*b-a<<'\n';
+}
