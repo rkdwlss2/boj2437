89d1889faae96b745497ddbdf983747cc0ea0ae6
rkdwlss2 <rkdwlss2@gmail.com>
2021-05-29 15:45:36 +0900

    boj17608

--- boj1976.cpp
+++ boj1976.cpp
@@ -1,65 +1,65 @@
+////
+////  boj1976.cpp
+////  boj2437
+////
+////  Created by 강명진 on 2021/05/28.
+////
 //
-//  boj1976.cpp
-//  boj2437
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//int n,m;
 //
-//  Created by 강명진 on 2021/05/28.
+//int graph[201][201];
 //
-
-#include <iostream>
-#include <vector>
-using namespace std;
-int n,m;
-
-int graph[201][201];
-
-int arr[1001];
-
-int parent[201];
-
-int findParent(int now){
-    if (parent[now]==now)return now;
-    return parent[now]=findParent(parent[now]);
-}
-
-void Union(int a,int b){
-    a=findParent(a);
-    b=findParent(b);
-    if (b>a)parent[b]=a;
-    else parent[a]=b;
-}
-
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin>>n;
-    cin>>m;
-    for (int i = 1 ; i <=n;i++){
-        parent[i]=i;
-    }
-    for (int i = 1 ; i <= n;i++){
-        for (int j = 1 ; j <=n;j++){
-            int tmp;
-            cin>>tmp;
-            if (tmp==1){
-                Union(i,j);
-            }
-        }
-    }
-    bool ok=false;
-    int prev=-1;
-    for (int i = 0 ; i < m;i++){
-        int tmp;
-        cin>>tmp;
-        int ans=findParent(tmp);
-        if (i!=0&&prev!=ans){
-            ok=true;
-            break;
-        }
-        prev=ans;
-    }
-    if (ok==true){
-        cout<<"NO"<<'\n';
-    }else{
-        cout<<"YES"<<'\n';
-    }
-}
+//int arr[1001];
+//
+//int parent[201];
+//
+//int findParent(int now){
+//    if (parent[now]==now)return now;
+//    return parent[now]=findParent(parent[now]);
+//}
+//
+//void Union(int a,int b){
+//    a=findParent(a);
+//    b=findParent(b);
+//    if (b>a)parent[b]=a;
+//    else parent[a]=b;
+//}
+//
+//int main(){
+//    ios::sync_with_stdio(0);
+//    cin.tie(0);
+//    cin>>n;
+//    cin>>m;
+//    for (int i = 1 ; i <=n;i++){
+//        parent[i]=i;
+//    }
+//    for (int i = 1 ; i <= n;i++){
+//        for (int j = 1 ; j <=n;j++){
+//            int tmp;
+//            cin>>tmp;
+//            if (tmp==1){
+//                Union(i,j);
+//            }
+//        }
+//    }
+//    bool ok=false;
+//    int prev=-1;
+//    for (int i = 0 ; i < m;i++){
+//        int tmp;
+//        cin>>tmp;
+//        int ans=findParent(tmp);
+//        if (i!=0&&prev!=ans){
+//            ok=true;
+//            break;
+//        }
+//        prev=ans;
+//    }
+//    if (ok==true){
+//        cout<<"NO"<<'\n';
+//    }else{
+//        cout<<"YES"<<'\n';
+//    }
+//}
---
+++ boj4195.cpp
@@ -0,0 +1,34 @@
+////
+////  boj4195.cpp
+////  boj2437
+////
+////  Created by 강명진 on 2021/05/29.
+////
+//
+//#include <iostream>
+//#include <unordered_map>
+//#include <string>
+//using namespace std;
+//
+//int main(){
+//    ios::sync_with_stdio(0);
+//    cin.tie(0);
+//    unordered_map<string,int> um;
+//    int tc;
+//    cin>>tc;
+//    int index=0;
+//    while(tc--){
+//        string a,b;
+//        cin>>a>>b;
+//        int A=-1;
+//        int B=-1;
+//        if (um.count(a)>0){
+//            A=um[a];
+//        }else{
+//            index++;
+//
+//        }
+//        if (um.count(b)>0)B=um[b];
+//
+//    }
+//}
