d8422eb38206f514a268292ad6729bf34914d802
rkdwlss2 <rkdwlss2@gmail.com>
2021-06-06 15:19:33 +0900

    boj1158

---
+++ boj1158.cpp
@@ -0,0 +1,36 @@
+//
+//  boj1158.cpp
+//  boj2437
+//
+//  Created by 강명진 on 2021/06/06.
+//
+
+#include <iostream>
+#include <queue>
+using namespace std;
+int n,k;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>n>>k;
+    queue<int> q;
+    for (int i = 1 ; i <=n;i++){
+        q.push(i);
+    }
+    cout<<'<';
+    for (int i = 0;i<n;i++){
+        int tmp=k;
+        int now=0;
+        while(tmp--){
+            now=q.front();
+            q.pop();
+            if (tmp!=0){
+                q.push(now);
+            }
+        }
+        cout<<now;
+        if (i!=n-1)cout<<", ";
+    }
+    cout<<'>';
+}
