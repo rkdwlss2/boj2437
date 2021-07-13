543056e7c2caab5e49273c4ec7488b8a7a665a25
rkdwlss2 <rkdwlss2@gmail.com>
2021-07-13 22:47:46 +0900

    boj9375

--- boj10987.cpp
+++ boj10987.cpp
@@ -1,23 +1,23 @@
+////
+////  boj10987.cpp
+////  boj2437
+////
+////  Created by 강명진 on 2021/07/04.
+////
 //
-//  boj10987.cpp
-//  boj2437
-//
-//  Created by 강명진 on 2021/07/04.
-//
-
-#include <iostream>
-#include <string>
-using namespace std;
-char arr[5]={'a','e','i','o','u'};
-int main(){
-    string str;
-    cin>>str;
-    int num=0;
-    for (int i = 0 ; i <str.size();i++){
-        for (int j = 0 ; j <5;j++){
-            if (str[i]==arr[j])num++;
-        }
-        
-    }
-    cout<<num<<'\n';
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//char arr[5]={'a','e','i','o','u'};
+//int main(){
+//    string str;
+//    cin>>str;
+//    int num=0;
+//    for (int i = 0 ; i <str.size();i++){
+//        for (int j = 0 ; j <5;j++){
+//            if (str[i]==arr[j])num++;
+//        }
+//        
+//    }
+//    cout<<num<<'\n';
+//}
