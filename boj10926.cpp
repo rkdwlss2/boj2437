8663374325e424dd2ce1dbe85c61e0ae976c296c
rkdwlss2 <61217289+rkdwlss2@users.noreply.github.com>
2021-10-29 22:11:25 +0900

    boj10926

---
+++ boj10926.cpp
@@ -0,0 +1,16 @@
+//
+//  boj10926.cpp
+//  boj2437
+//
+//  Created by 강명진 on 2021/10/29.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string tmp;
+    cin>>tmp;
+    cout<<tmp<<"??!"<<'\n';
+}
